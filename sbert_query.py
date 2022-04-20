101026add95d4ce335ffefb30a3c02483299c9e5
izmaxxsun <isra.sunhachawi@elastic.co>
2022-04-20 15:36:45 +0000

    Update sbert indexer to new index

--- sbert_query.py
+++ sbert_query.py
@@ -10,7 +10,7 @@ sentence_transformer = SentenceTransformer("all-mpnet-base-v2")
 
 es_client = Elasticsearch(cloud_id=os.environ['CLOUD_ID'], api_key=os.environ['CLOUD_API_KEY'])
 
-INDEX_NAME = "my-approx-knn-index"
+INDEX_NAME = "faq-knn-sbert"
 
 def query_question(question: str, top_n: int = 10) -> List[dict]:
 
