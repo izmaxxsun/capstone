abe91c08b8c531b9bc6bd1e5dc3c3464640c8dd5
izmaxxsun <isra.sunhachawi@elastic.co>
2022-04-20 11:14:45 -0400

    Add env variables for indexer

--- sbert_indexer.py
+++ sbert_indexer.py
@@ -2,25 +2,32 @@ import csv
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 from elasticsearch import Elasticsearch
+from pathlib import Path
+import os
 
-INDEX_NAME = "my-approx-knn-index"
+base_path = Path(__file__).parent
+file_path = (base_path / "../source_data/faq_extract_sbert.csv").resolve()
+
+INDEX_NAME = "faq-knn-sbert"
 
 sentence_transformer = SentenceTransformer("all-mpnet-base-v2")
 
-es_client = Elasticsearch(cloud_id="My_deployment:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyQ2MGUxMWIyZGMyZTY0NjhmYjM1ODQ3MjJhZjVjOTUyNyRjZmMzYzVlNTRmZjM0ZjE2OGY4MmFmODI1MjBhZTZkMg==",
-                          api_key="a01tNV8zOEJ5ZUE5VTkzUnpjLXc6bHlvUlhxNnBSY2FlNzhrcXo3OXdmUQ==")
+es_client = Elasticsearch(cloud_id=os.environ['CLOUD_ID'],
+                          api_key=os.environ['CLOUD_API_KEY'])
 
 with open('faq_extract_sbert.csv') as f:
     reader = csv.DictReader(f)
     for row in reader:
         question = row['Question']
         answer = row['Answer']
+        source = row['Source']
         embedding = sentence_transformer.encode(question)
 
         data = {
             "question": question,
             "sbert_encoding": embedding,
             "answer": answer,
+            "source": source
         }
 
         response = es_client.index(
