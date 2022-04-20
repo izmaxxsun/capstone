72fdae506a343931de0a9936433abb486c47c7ba
izmaxxsun <isra.sunhachawi@elastic.co>
2022-04-20 10:39:50 -0400

    add drug costs screenshot

--- index_helpers/medicare_part_d_indexer.py
+++ index_helpers/medicare_part_d_indexer.py
@@ -1,24 +1,31 @@
 from elasticsearch import Elasticsearch, helpers
 import csv
 import json
+import os
+from pathlib import Path
 
-es = Elasticsearch(cloud_id="My_deployment:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyQ2MGUxMWIyZGMyZTY0NjhmYjM1ODQ3MjJhZjVjOTUyNyRjZmMzYzVlNTRmZjM0ZjE2OGY4MmFmODI1MjBhZTZkMg==",api_key="a01tNV8zOEJ5ZUE5VTkzUnpjLXc6bHlvUlhxNnBSY2FlNzhrcXo3OXdmUQ==")
+INDEX_NAME = 'drugs-2019'
+base_path = Path(__file__).parent
+file_path = (base_path / "../source_data/medicare_part_d_cms/Medicare_Part_D_Prescribers_by_Geography_and_Drug_2019.csv").resolve()
+
+es = Elasticsearch(cloud_id=os.environ['CLOUD_ID'],api_key=os.environ['CLOUD_API_KEY'])
 result = es.ping()
 print(result)
 if result:
     print("Connected to Elasticsearch")
+    with open(file_path) as f:
+        reader = csv.DictReader(f)
+        
+        try:
+            resp = helpers.bulk(es, reader, index=INDEX_NAME)
+            print ("helpers.bulk() RESPONSE:", resp)
+            print ("helpers.bulk() RESPONSE:", json.dumps(resp, indent=4))
+        except helpers.BulkIndexError as bulkIndexError:
+            print("Indexing error: {0}".format(bulkIndexError))
 else:
     print("Not connected")
 
-with open('Medicare_Part_D_Prescribers_by_Geography_and_Drug_2019.csv') as f:
-    reader = csv.DictReader(f)
-    
-    try:
-        resp = helpers.bulk(es, reader, index='drugs-2019-a')
-        print ("helpers.bulk() RESPONSE:", resp)
-        print ("helpers.bulk() RESPONSE:", json.dumps(resp, indent=4))
-    except helpers.BulkIndexError as bulkIndexError:
-        print("Indexing error: {0}".format(bulkIndexError))
+
 
 
 
